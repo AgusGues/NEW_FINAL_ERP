6d54e7178dbdab9de73d9e952e2807e0cf679a5c
Agus <agusgus612@gmail.com>
2026-02-21 00:08:26 +0700

    add itemsUOM.cs

---
+++ NEW_FINAL_ERP/Models/ItemsUom.cs
@@ -0,0 +1,27 @@
+﻿namespace NEW_FINAL_ERP.Models
+{
+    public class ItemsUom
+    {
+        public int ItemUOMId { get; set; }
+
+        public int ItemId { get; set; }
+        public int UnitId { get; set; }
+
+        public decimal ConversionToBase { get; set; }
+
+        public bool IsBase { get; set; }
+        public bool IsDefaultSales { get; set; }
+        public bool IsDefaultPurchase { get; set; }
+
+        public string? Barcode { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+
+        
+
+
+    }
+}
